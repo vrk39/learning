ccf8378971fce2925ecaaa7b15bc3f3331eb4331
vrk39 <kansevinayak1993@gmail.com>
2026-08-01 06:15:21 +0530

    Update docusaurus.config.js

--- learning-wiki/docusaurus.config.js
+++ learning-wiki/docusaurus.config.js
@@ -30,7 +30,7 @@ const config = {
   organizationName: 'vrk39', // Usually your GitHub org/user name.
   projectName: 'learning', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
