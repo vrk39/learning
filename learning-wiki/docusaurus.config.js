f7589b72a339be97e5e59a9f62348bd8d551f7dc
vrk39 <kansevinayak1993@gmail.com>
2026-08-01 06:06:30 +0530

    Update docusaurus.config.js

--- learning-wiki/docusaurus.config.js
+++ learning-wiki/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://https://vrk39.github.io',
+  url: 'https://vrk39.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/learning/',
